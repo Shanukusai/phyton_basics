7593db41090d3e51de39d35ec6e0da8c1012df9c
Margo <ishanikusai@gmail.com>
2019-12-26 00:28:19 +0300

    lesson01

--- lesson01/task0105.py
+++ lesson01/task0105.py
@@ -5,11 +5,19 @@
 # Если фирма отработала с прибылью, вычислите рентабельность выручки (соотношение прибыли к выручке).
 # Далее запросите численность сотрудников фирмы и определите прибыль фирмы в расчете на одного сотрудника.
 
+print('Данная программа делает расчеты некоторых финансовых показателей.')
 revenue = int(input('Внесите значние выручки: '))
 costs = int(input('Внесите значние издержек: '))
 
 profit_or_loss = revenue - costs
 
-profitability_of_revenue
-number_of_employees
-profit_per_employee
+if profit_or_loss > 0:
+    print(f'Поздравляю! Ваша прибыль составила: {profit_or_loss}')
+    print(f'Ваша рентабельность: {profit_or_loss / revenue:.2%}')
+    number_of_employees = int(
+        input('Введите численность сотрудников для растча размера прибыли на одного сотрудника: '))
+    print(f'Прибыль на одного сотрудика: {profit_or_loss / number_of_employees:.2f}')
+elif profit_or_loss == 0:
+    print(f'Вы ничего не заработали, но и не потеряли. Ваша прибыль равна {profit_or_loss}')
+else:
+    print(f'Сочувствую! Ваш убыток составил: {profit_or_loss}')
